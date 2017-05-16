325037aa589e2d49da9087e25493ed4e55c0713a
Kyle Welch <kwelch0626@gmail.com>
2017-05-16 08:03:47 -0500

    🚢 Added deploy scripts

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: '[name].[hash].js',
     path: path.resolve('./dist'),
-    publicPath: '/',
+    publicPath: '/family-feud-game/',
   },
   module: {
     rules: [
