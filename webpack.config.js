f76a518135bd2961d399e170f2bd09f8bb7e2626
Kyle Welch <kwelch0626+github@gmail.com>
2017-05-17 13:39:51 -0500

    Added env output to build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env = {}) => ({
+module.exports = (env = {}) => (console.log('isProduction:', env.production) || {
   target: 'web',
   devtool: 'cheap-eval-source-map',
   entry: {
